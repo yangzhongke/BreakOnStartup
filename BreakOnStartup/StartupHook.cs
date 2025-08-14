53963ef5c6006601996e9b28c52e869722ed0de5
杨中科 <yangzhongke8@gmail.com>
2025-08-14 20:21:09 +1200

    1. Add IntelliJ IDEA project files to .gitignore 2. Add StartupHook

---
+++ BreakOnStartup/StartupHook.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+
+public static class StartupHook
+{
+    public static void Initialize()
+    {
+        string targetAssemblyName = Environment.GetEnvironmentVariable("DOTNET_STARTUP_HOOKS_TARGET_ASSEMBLY_NAME");
+        string entryAssemblyName = Assembly.GetEntryAssembly()?.GetName().Name;
+        if (targetAssemblyName != null && targetAssemblyName == entryAssemblyName)
+        {
+            Debugger.Launch();
+        }
+    }
+}
